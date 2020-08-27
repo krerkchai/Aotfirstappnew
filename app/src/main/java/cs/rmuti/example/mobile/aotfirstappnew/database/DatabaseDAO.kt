9141cf6cf986e52a4181abf7bef40ae4123d3ba5
USER <ppopp9450@gmail.com>
2020-08-27 16:01:41 +0700

    Create the DAO

---
+++ app/src/main/java/cs/rmuti/example/mobile/aotfirstappnew/database/DatabaseDAO.kt
@@ -0,0 +1,15 @@
+package cs.rmuti.example.mobile.aotfirstappnew.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import chimjan.xkalux.mobilehomework_firstapp.database.Contact
+
+@Dao
+interface DatabaseDAO {
+    @Insert
+    fun insert(contact: Contact)
+    @Query("SELECT * from contact_table")
+    fun get(): LiveData<List<Contact>>
+}
